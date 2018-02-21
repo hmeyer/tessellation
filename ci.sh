b6c6536808ff47ba386d381d7cc98173838ceb04
Henning Meyer <tutmann@gmail.com>
2018-02-21 22:29:48 +0100

    fix permissions and tests

--- ci.sh
+++ ci.sh
@@ -6,4 +6,10 @@ apt update
 apt upgrade -y
 apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libgtk-3-dev
 
+cargo build
 cargo test
+cargo bench
+cd stl_io; cargo build; cargo test; cd ..
+cd luascad; cargo build; cargo test; cargo bench; cd ..
+cd primitive; cargo build; cargo test; cargo bench; cd ..
+cd tessellation; cargo build; cargo test; cargo bench; cd ..
