e436090cdfcd56e238025150a2602306d62d3e39
Henning Meyer <tutmann@gmail.com>
2018-02-21 23:18:17 +0100

    make ci.sh fail

--- ci.sh
+++ ci.sh
@@ -6,9 +6,11 @@ apt update
 apt upgrade -y
 apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libgtk-3-dev
 
+set -e
 cargo build
 cargo test
 cargo bench
+cd foobar
 cd luascad; cargo build; cargo test; cargo bench; cd ..
 cd primitive; cargo build; cargo test; cargo bench; cd ..
 cd tessellation; cargo build; cargo test; cargo bench; cd ..
