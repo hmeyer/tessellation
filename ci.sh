a2d4cd2d1db885156e1cdc12e3680b95f36a7425
Henning Meyer <tutmann@gmail.com>
2018-02-21 23:14:47 +0100

    fix ci.sh - stl_io does not exist anymore here

--- ci.sh
+++ ci.sh
@@ -9,7 +9,6 @@ apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev
 cargo build
 cargo test
 cargo bench
-cd stl_io; cargo build; cargo test; cd ..
 cd luascad; cargo build; cargo test; cargo bench; cd ..
 cd primitive; cargo build; cargo test; cargo bench; cd ..
 cd tessellation; cargo build; cargo test; cargo bench; cd ..
