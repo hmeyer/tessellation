a36ba342c76333f960ca8213a86b549dc9ac83e5
Henning Meyer <tutmann@gmail.com>
2018-03-05 20:43:00 +0100

    fix travis

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ echo 'deb http://debian.ethz.ch/debian stretch main contrib' >> /etc/apt/sources
 
 apt update
 apt upgrade -y
-apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libgtk-3-dev
+apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libgtk-3-dev libgtksourceview-3.0-dev
 
 set -e
 cargo build
